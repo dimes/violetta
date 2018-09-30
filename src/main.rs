e79ae442defa9065bb9fcdc2eb8f4dac60e72793
Dimes <mgale4@binghamton.edu>
2018-09-30 01:21:14 -0400

    Remove position on screen

--- src/main.rs
+++ src/main.rs
@@ -57,13 +57,6 @@ fn main() {
     game_runner.start();
 }
 
-struct Position {
-    center_x: f32,
-    center_y: f32,
-    width: f32,
-    height: f32,
-}
-
 struct GameBox {
     obj: GameObj,
     direction: (f32, f32),
@@ -124,7 +117,7 @@ struct GameObj {
 impl GameObj {
     fn new(context: &mut Context) -> GameObj {
         let mut entity = entities::Entity::new(32);
-        let mut renderable = Renderable::new();
+        let renderable = Renderable::new();
         entity.set_component::<Renderable>(renderable);
         let key = context.entities.add(Box::new(entity));
         return GameObj { key };
@@ -159,31 +152,6 @@ impl GameObj {
             .and_then({ |entity| entity.get_component::<Renderable>(Renderable::name()) })
         {
             renderable.set_position(x, y);
-        }
-    }
-
-    /// Returns the position of this object, relative to the top left corner of the screen.
-    fn position_on_screen(&mut self, context: &mut Context, out_position: &mut Position) {
-        if let Some(renderable) = context
-            .entities
-            .get(&self.key)
-            .and_then({ |entity| entity.get_component::<Renderable>(Renderable::name()) })
-        {
-            let screen = &context.screen;
-            let camera = &context.camera;
-
-            let pixels_per_world_width = screen.width as f32 / (camera.right - camera.left);
-            let pixels_per_world_height = screen.height as f32 / (camera.bottom - camera.top);
-
-            let center_x = (renderable.x - camera.left) * pixels_per_world_width;
-            let center_y = (renderable.y - camera.top) * pixels_per_world_height;
-            let width = renderable.width * pixels_per_world_width;
-            let height = renderable.height * pixels_per_world_height;
-
-            out_position.center_x = center_x;
-            out_position.center_x = center_y;
-            out_position.width = width;
-            out_position.height = height;
         }
     }
 }
